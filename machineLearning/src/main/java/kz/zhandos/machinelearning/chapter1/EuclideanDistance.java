7683a8c2c9db6281aac6afb814fef9ea9c157a3d
zhandos <zhandos22.03.90@gmail.com>
2015-12-01 17:49:10 +0600

    added topMatch, recommendation methods to EuclideanDistance

--- machineLearning/src/main/java/kz/zhandos/machinelearning/chapter1/EuclideanDistance.java
+++ machineLearning/src/main/java/kz/zhandos/machinelearning/chapter1/EuclideanDistance.java
@@ -1,26 +1,77 @@
 package kz.zhandos.machinelearning.chapter1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EuclideanDistance {
 
-	public static double distance(Map<String, Map<String, Double>> critics, String critic1, String critic2) {
-		double ret = 0;
-		List<String> commonFilms = new ArrayList<String>();
-		Map<String, Double> films2 = critics.get(critic2);
-		Map<String, Double> films1 = critics.get(critic1);
-		for (String film : critics.get(critic1).keySet()) {
-			if (films2.containsKey(film))
-				commonFilms.add(film);
-		}
-		if (commonFilms.size() > 0) {
-			for (String film : commonFilms) {
-				ret += Math.pow(films1.get(film) - films2.get(film), 2);
-			}
-			ret = 1 / (1 + ret);
-		}
-		return ret;
-	}
+  public static double distance(Map<String, Map<String, Double>> critics, String critic1,
+      String critic2) {
+    double ret = 0;
+    List<String> commonFilms = new ArrayList<String>();
+    Map<String, Double> films2 = critics.get(critic2);
+    Map<String, Double> films1 = critics.get(critic1);
+    for (String film : critics.get(critic1).keySet()) {
+      if (films2.containsKey(film))
+        commonFilms.add(film);
+    }
+    if (commonFilms.size() > 0) {
+      for (String film : commonFilms) {
+        ret += Math.pow(films1.get(film) - films2.get(film), 2);
+      }
+      ret = 1 / (1 + ret);
+    }
+    return ret;
+  }
+
+
+  public static Map<String, Double> topMathes(Map<String, Map<String, Double>> critics,
+      String critic, int number) {
+    Map<String, Double> ret = new HashMap<String, Double>();
+    for (String person : critics.keySet()) {
+      if (!person.equalsIgnoreCase(critic))
+        ret.put(person, distance(critics, critic, person));
+    }
+
+    return ret;
+  }
+
+  public static Map<String, Double> recomendations(Map<String, Map<String, Double>> critics,
+      String critic) {
+    Map<String, Double> ret = new HashMap<String, Double>();
+    for (String person : critics.keySet()) {
+      if (!person.equalsIgnoreCase(critic)) {
+        double sim = distance(critics, critic, person);
+        if (sim < 0)
+          continue;
+
+      }
+    }
+    return ret;
+  }
+
+
+  public static Map<String, Map<String, Double>> calculateSimilarItems(
+      Map<String, Map<String, Double>> movies) {
+    Map<String, Map<String, Double>> ret = new HashMap<String, Map<String, Double>>();
+    // c=0
+    // for item in itemPrefs:
+    // # Status updates for large datasets
+    // c+=1
+    // if c%100==0: print "%d / %d" % (c,len(itemPrefs))
+    // # Find the most similar items to this one
+    // scores=topMatches(itemPrefs,item,n=n,similarity=sim_distance)
+    // result[item]=scores
+    int c = 0;
+    for (Map.Entry<String, Map<String, Double>> e : movies.entrySet()) {
+      c++;
+      if (c % 100 == 0)
+        System.out.println(c + " / " + e.getValue().size());
+      ret.put(e.getKey(), topMathes(movies, e.getKey(), 5));
+    }
+
+    return ret;
+  }
 }
--- machineLearning/src/test/java/kz/zhandos/machinelearning/chapter1/EuclideanDistanceTest.java
+++ machineLearning/src/test/java/kz/zhandos/machinelearning/chapter1/EuclideanDistanceTest.java
@@ -4,14 +4,23 @@ import static org.fest.assertions.api.Assertions.assertThat;
 
 import org.testng.annotations.Test;
 
-import kz.zhandos.machinelearning.chapter1.Db;
-import kz.zhandos.machinelearning.chapter1.EuclideanDistance;
-
 public class EuclideanDistanceTest {
 
-	@Test(timeOut = 1000)
-	public void mainTest() {
-		assertThat(EuclideanDistance.distance(Db.CRITICS, "Lisa Rose", "Gene Seymour")).isEqualTo(0.14814814814814814d);
-	}
+  @Test(timeOut = 1000)
+  public void mainTest() {
+    assertThat(EuclideanDistance.distance(Db.CRITICS, "Lisa Rose", "Gene Seymour"))
+        .isEqualTo(0.14814814814814814d);
+  }
+
+  @Test(timeOut = 1000)
+  public void topTest() {
+    System.out.println(EuclideanDistance.topMathes(Db.CRITICS, "Toby", 5));
+  }
+
+  @Test(timeOut = 1000)
+  public void itemTest() {
+    System.out.println(EuclideanDistance.calculateSimilarItems(Db.MOVIES));
+  }
+
 
 }
